62b04477d51de5b37cb7b1bf9834908afea7e664
FabianQF <fquirosf@ucenfotec.ac.cr>
2017-08-03 12:22:30 -0600

    Arrglos de taps en patrocinadores

--- components/sponsors/sponsor.controller.js
+++ components/sponsors/sponsor.controller.js
@@ -12,16 +12,15 @@
       }init();//Fin función
 
       $scope.pagina = 1;
-        $scope.siguiente = function() {
-          $scope.pagina++;
-        }
-
-        $scope.anterior = function() {
-          $scope.pagina--;
-        }
-        $scope.registro1 = function() {
-          $scope.pagina = 1;
-        }
+      $scope.siguiente = function() {
+        $scope.pagina = 2;
+      }
+      $scope.anterior = function() {
+        $scope.pagina = 1;
+      }
+      $scope.registro1 = function() {
+        $scope.pagina = 1;
+      }
 
       // Inicio de la función presave
           vm.presave= function(newSponsor){
